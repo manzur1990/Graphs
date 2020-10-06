7662e4b3459f84f689271df50a3969a0338bd9f9
JM <manzur-1990@hotmail.com>
2020-10-06 15:13:47 -0500

    Day 1

--- projects/graph/test_graph.py
+++ projects/graph/test_graph.py
@@ -3,6 +3,7 @@ import sys
 import io
 from graph import Graph
 
+
 class Test(unittest.TestCase):
     def setUp(self):
         self.graph = Graph()
@@ -14,7 +15,7 @@ class Test(unittest.TestCase):
         self.graph.add_vertex(5)
         self.graph.add_vertex(6)
         self.graph.add_vertex(7)
-        
+
         self.graph.add_edge(5, 3)
         self.graph.add_edge(6, 3)
         self.graph.add_edge(7, 1)
@@ -28,30 +29,24 @@ class Test(unittest.TestCase):
 
     def test_vertices(self):
         vertices = {
-          1: {2},
-          2: {3, 4},
-          3: {5},
-          4: {6, 7}, 
-          5: {3},
-          6: {3},
-          7: {1, 6}
+            1: {2},
+            2: {3, 4},
+            3: {5},
+            4: {6, 7},
+            5: {3},
+            6: {3},
+            7: {1, 6}
         }
         self.assertDictEqual(self.graph.vertices, vertices)
 
     def test_bft(self):
         bft = [
-            "1\n2\n3\n4\n5\n6\n7\n",
-            "1\n2\n3\n4\n5\n7\n6\n",
-            "1\n2\n3\n4\n6\n7\n5\n",
-            "1\n2\n3\n4\n6\n5\n7\n",
-            "1\n2\n3\n4\n7\n6\n5\n",
-            "1\n2\n3\n4\n7\n5\n6\n",
-            "1\n2\n4\n3\n5\n6\n7\n",
-            "1\n2\n4\n3\n5\n7\n6\n",
-            "1\n2\n4\n3\n6\n7\n5\n",
-            "1\n2\n4\n3\n6\n5\n7\n",
-            "1\n2\n4\n3\n7\n6\n5\n",
-            "1\n2\n4\n3\n7\n5\n6\n"
+            "1\n2\n3\n4\n5\n6\n7\n", "1\n2\n3\n4\n5\n7\n6\n",
+            "1\n2\n3\n4\n6\n7\n5\n", "1\n2\n3\n4\n6\n5\n7\n",
+            "1\n2\n3\n4\n7\n6\n5\n", "1\n2\n3\n4\n7\n5\n6\n",
+            "1\n2\n4\n3\n5\n6\n7\n", "1\n2\n4\n3\n5\n7\n6\n",
+            "1\n2\n4\n3\n6\n7\n5\n", "1\n2\n4\n3\n6\n5\n7\n",
+            "1\n2\n4\n3\n7\n6\n5\n", "1\n2\n4\n3\n7\n5\n6\n"
         ]
 
         stdout_ = sys.stdout
@@ -65,10 +60,8 @@ class Test(unittest.TestCase):
 
     def test_dft(self):
         dft = [
-            "1\n2\n3\n5\n4\n6\n7\n",
-            "1\n2\n3\n5\n4\n7\n6\n",
-            "1\n2\n4\n7\n6\n3\n5\n",
-            "1\n2\n4\n6\n3\n5\n7\n"
+            "1\n2\n3\n5\n4\n6\n7\n", "1\n2\n3\n5\n4\n7\n6\n",
+            "1\n2\n4\n7\n6\n3\n5\n", "1\n2\n4\n6\n3\n5\n7\n"
         ]
 
         stdout_ = sys.stdout
@@ -82,10 +75,8 @@ class Test(unittest.TestCase):
 
     def test_dft_recursive(self):
         dft = [
-            "1\n2\n3\n5\n4\n6\n7\n",
-            "1\n2\n3\n5\n4\n7\n6\n",
-            "1\n2\n4\n7\n6\n3\n5\n",
-            "1\n2\n4\n6\n3\n5\n7\n"
+            "1\n2\n3\n5\n4\n6\n7\n", "1\n2\n3\n5\n4\n7\n6\n",
+            "1\n2\n4\n7\n6\n3\n5\n", "1\n2\n4\n6\n3\n5\n7\n"
         ]
 
         stdout_ = sys.stdout
@@ -102,18 +93,13 @@ class Test(unittest.TestCase):
         self.assertListEqual(self.graph.bfs(1, 6), bfs)
 
     def test_dfs(self):
-        dfs = [
-            [1, 2, 4, 6],
-            [1, 2, 4, 7, 6]
-        ]
-        self.assertIn(self.graph.dfs(1,6), dfs)
+        dfs = [[1, 2, 4, 6], [1, 2, 4, 7, 6]]
+        self.assertIn(self.graph.dfs(1, 6), dfs)
 
     def test_dfs_recursive(self):
-        dfs = [
-            [1, 2, 4, 6],
-            [1, 2, 4, 7, 6]
-        ]
-        self.assertIn(self.graph.dfs_recursive(1,6), dfs)
+        dfs = [[1, 2, 4, 6], [1, 2, 4, 7, 6]]
+        self.assertIn(self.graph.dfs_recursive(1, 6), dfs)
+
 
 if __name__ == '__main__':
     unittest.main()
